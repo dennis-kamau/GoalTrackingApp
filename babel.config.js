ada9d9b914cfa83034c3c8bf3f9b97b37235f3ce
Dennis Kamau <kamaukiniiya@gmail.com>
2023-02-27 12:57:02 +0300

    Setup path aliases

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,37 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+module.exports = function (
+  /** @type {{ cache: (arg0: boolean) => void; }} */ api,
+) {
+  api.cache(true);
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          extensions: [
+            '.ios.js',
+            'ios.tsx',
+            '.android.js',
+            'android.tsx',
+            '.js',
+            '.ts',
+            '.tsx',
+            '.json',
+          ],
+          alias: {
+            '@GoalTrackingApp': './src',
+            '@GoalTrackingApp/components': './src/components',
+            '@GoalTrackingApp/hooks': './src/hooks',
+            '@GoalTrackingApp/models': './src/models',
+            '@GoalTrackingApp/navigation': './src/navigation',
+            '@GoalTrackingApp/redux': './src/redux',
+            '@GoalTrackingApp/screens': './src/screens',
+            '@GoalTrackingApp/theme': './src/theme',
+            '@GoalTrackingApp/utils': './src/utils',
+          },
+        },
+      ],
+    ],
+  };
 };
